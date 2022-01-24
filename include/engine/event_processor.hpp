436240f19457870a2e402b103c8210661a29f3f1
riezenmark <riezenmark@github.git>
2022-01-25 07:47:37 +1000

    sketch: event_processor module

--- include/engine/event_processor.hpp
+++ include/engine/event_processor.hpp
@@ -3,6 +3,13 @@
 // Class for keyboard and controller events processing.
 class EventProcessor {
 private:
+  ////////////////////////////
+  // controller type maybe? //
+  ////////////////////////////
+
+  /////////////////////////////////////////
+  // some glfw_button to real_button map //
+  /////////////////////////////////////////
 public:
   // @brief Creates event processor.
   EventProcessor();
